5fcadde5732c595a13357e780b2533f33cafda44
jtabach <jeff.n.tabachnick@gmail.com>
2017-05-20 16:19:35 -0700

    create Movie class and create a Movie instance

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,2 +1,7 @@
 import media
 
+remember_the_titans = media.Movie("Remember The Titans",
+                            "A 2000 American sports film produced by Jerry Bruckheimer and directed by Boaz Yakin",
+                            "https://en.wikipedia.org/wiki/File:Remember_the_titansposter.jpg",
+                            "https://www.youtube.com/watch?v=nPhu9XsRl4M")
+print(remember_the_titans.storyline)
