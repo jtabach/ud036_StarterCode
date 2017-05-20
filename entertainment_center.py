362bb33fdb41a7be2f9be8644cbe0c4ea6eee313
jtabach <jeff.n.tabachnick@gmail.com>
2017-05-20 16:23:31 -0700

    use fresh tomatoes to create an html file

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,7 +1,12 @@
 import media
+import fresh_tomatoes
 
 remember_the_titans = media.Movie("Remember The Titans",
                             "A 2000 American sports film produced by Jerry Bruckheimer and directed by Boaz Yakin",
-                            "https://en.wikipedia.org/wiki/File:Remember_the_titansposter.jpg",
+                            "https://upload.wikimedia.org/wikipedia/en/d/d1/Remember_the_titansposter.jpg",
                             "https://www.youtube.com/watch?v=nPhu9XsRl4M")
 print(remember_the_titans.storyline)
+
+movie_list = [remember_the_titans]
+
+fresh_tomatoes.open_movies_page(movie_list)
